cfce24c72bb77f3302635283463f755e0b4d5bfe
andsim04 <simkovic4@stud.uniza.sk>
2024-01-02 12:51:59 +0100

    Zalozenie

---
+++ cmake-build-debug/CMakeFiles/POS_SEM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POS_SEM.dir/main.c.o"
+  "POS_SEM"
+  "POS_SEM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POS_SEM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
